5f397e5f12bb87ad391dab821b0a1b1d51c5201d
Francisco Avila <franciscoavila@Franciscos-MBP.home>
2022-08-20 08:25:44 -0700

    url paths and views comp but not tested

--- back_end/back_app/urls.py
+++ back_end/back_app/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('sign_up', views.sign_up, name='sign_up'),
     path('sign_in', views.log_in, name='sign_in'),
     path('sign_out', views.log_out, name='sign_out'),
-    path('curr_user', views.curr_user, name='curr_user')
+    path('curr_user', views.curr_user, name='curr_user'),
+    path('profile_page', views.profile_page, name='profile_page'),
+    path('jobs', views.jobs_applied_for, name='jobs_of_user'),
+    path('interviews', views.interviews, name='interviews_of_user'),
+    path('forums', views.forums, name='forums_for_user')
 ]
--- back_end/back_app/views.py
+++ back_end/back_app/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.core import serializers
+
 
 # Create your views here.
 
@@ -58,7 +60,87 @@ def log_out(request):
 def curr_user(request):
     if request.user.is_authenticated:
         data = serializers.serialize("json", [request.user], fields=[
-                                     'name', 'email', 'password'])
+                                     'first_name', 'email', 'password'])
         return HttpResponse(data)
     else:
         return JsonResponse({'user': None})
+
+@api_view(['GET'])
+def profile_page(request):
+    current_user= AppUser.objects.filter(id=request.user.id).values()
+    return Response(current_user[0])
+
+@api_view(['GET', 'POST'])
+def jobs_applied_for(request):
+    if request.method == "GET":
+        current= AppliedJobs.objects.filter(user=request.user.id).values()
+        return Response(list(current))
+    if request.method == 'POST':
+        try:
+            user=request.user
+            company_link=request.data['link']
+            job_description= request.data['description']
+            company_name=request.data['company_name']
+            job_title=request.data['job_title']
+            new_job=AppliedJobs.objects.create(user= user, company_link= company_link, description= job_description, company_name= company_name, job_title=job_title)
+            new_job.save()
+            if request.data['deadline']:
+                deadline=request.data['deadline']
+                new_job(deadline= deadline)
+                new_job.save()
+            if request.data['date_completed']:
+                date_comp=request.data['date_completed']
+                new_job(date_completed = date_comp)
+                new_job.save()
+            if request.data['completed']:
+                completed=request.data['completed']
+                new_job(completed= completed)
+                new_job.save()
+            return Response({"message": "success"})
+        except:
+            return Response({"message": "failed to post new job application"})
+        
+@api_view(["GET", "POST"])
+def interviews(request):
+    if request.method == "GET":
+        current= Interview.objects.filter(user=request.user.id).values()
+        return Response(list(current))
+    if request.method == "POST":
+        try:
+            user= request.user
+            company_name= request.data['company_name']
+            interview=Interview.objects.create(user=user, company_name= company_name)
+            interview.save() 
+            if request.data['interview_date']:
+                interview_date= request.data["interview_date"]
+                interview(interview_date=interview_date)
+                interview.save()
+            if request.data['completed']:
+                completed = request.data['completed']
+                interview(completed= completed)
+                interview.save()
+            return Response({"message": "new post of job interview"})
+        except:
+            return Response({"message": "interview creation failed"})
+        
+        
+        
+        
+        
+@api_view(["GET", "POST"])
+def forums(request):
+    if request.method == "GET":
+        current= Forum.objects.all()
+        return Response(list(current))
+    if request.method == "POST":
+        try:
+            title= request.data['title']
+            user= request.user
+            company_name=request.data['company_name']
+            job_title= request.data['job_title']
+            description=request.data['description']
+            forum=Forum.objects.create(title=title, user=user, company_name= company_name, job_title = job_title, description = description)
+            forum.save()
+            return Response({"message": "new post of job forum"})
+        except:
+            return Response({"message": "forum creation failed"})
