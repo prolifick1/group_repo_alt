b97ba47a605491026760ad35eba2e086f56fd8bd
Dennis Corral <dennis.corral@outlook.com>
2022-08-20 17:49:28 -0700

    add button added, ready for back end

--- front_end/src/pages/Dashboard.jsx
+++ front_end/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import React,{ useState } from "react"
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from "mdbreact";
@@ -10,6 +10,12 @@ function Dashboard(props) {
     const [jobTitle, setTitle] = useState('')
     const [description, setDescription] = useState('')
     const [companyLink, setCompanyLink] = useState('')
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+       };
 
     console.log(companyName)
     console.log(jobTitle)
@@ -31,7 +37,7 @@ function Dashboard(props) {
     return (
         <div>
 
-            <div>
+            {/* <div>
                 <button>Test Button</button>
                 <form onSubmit={() => addJob()} action="">
                     <input type="text" placeholder="Comapny Name" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
@@ -42,58 +48,18 @@ function Dashboard(props) {
                 </form>
 
 
-            </div>
-            {/* <React.Fragment>
+            </div> */}
+            <React.Fragment>
                 <MDBContainer>
                     <MDBRow>
                         <MDBCol md="9" className="mb-r">
-                            <h2 className="text-uppercase my-3">Today:</h2>
-                            <div id="events">
-                                {events.map(event => (
-                                    <Event
-                                        key={event.id}
-                                        id={event.id}
-                                        time={event.time}
-                                        title={event.title}
-                                        location={event.location}
-                                        description={event.description}
-                                        onDelete={handleDelete}
-                                    />
-                                ))}
-                            </div>
                             <MDBRow className="mb-4">
                                 <MDBCol xl="3" md="6" className="mx-auto text-center">
                                     <MDBBtn color="info" rounded onClick={toggleModal}>
-                                        Add Event
+                                        Add Job
                                     </MDBBtn>
                                 </MDBCol>
                             </MDBRow>
-                        </MDBCol>
-                        <MDBCol md="3">
-                            <h3 className="text-uppercase my-3">Schedule</h3>
-                            <h6 className="my-3">
-                                It is going to be busy today. You have{" "}
-                                <b>{events.length} events </b>.
-                            </h6>
-                            <h1 className="my-3">
-                                <MDBRow>
-                                    <MDBCol xs="3" className="text-center">
-                                        <MDBIcon icon="sun" fixed />
-                                    </MDBCol>
-                                    <MDBCol xs="9">Sunny</MDBCol>
-                                </MDBRow>
-                                <MDBRow>
-                                    <MDBCol xs="3" className="text-center">
-                                        <MDBIcon icon="thermometer-three-quarters" fixed />
-                                    </MDBCol>
-                                    <MDBCol xs="9">23°C</MDBCol>
-                                </MDBRow>
-                            </h1>
-                            <p>
-                                Don't forget your rain jacket. Today will warm and humid, becoming
-                                warm in the afternoon with temperatures between 65 and 73
-                                degrees F in Chicago.
-                            </p>
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
@@ -103,48 +69,14 @@ function Dashboard(props) {
                         titleClass="w-100 font-weight-bold"
                         toggle={toggleModal}
                     >
-                        Add new event
+                        Add new job
                     </MDBModalHeader>
                     <MDBModalBody>
                         <form className="mx-3 grey-text">
-                            <MDBInput
-                                name="time"
-                                label="Time"
-                                icon="clock"
-                                hint="12:30"
-                                group
-                                type="text"
-                                value={time}
-                                onChange={(e) => setTime(e.target.value)}
-                            />
-                            <MDBInput
-                                name="title"
-                                label="Title"
-                                icon="edit"
-                                hint="Briefing"
-                                group
-                                type="text"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                            <MDBInput
-                                name="location"
-                                label="Location (optional)"
-                                icon="map"
-                                group
-                                type="text"
-                                value={location}
-                                onChange={(e) => setLocation(e.target.value)}
-                            />
-                            <MDBInput
-                                name="description"
-                                label="Description (optional)"
-                                icon="sticky-note"
-                                group
-                                type="textarea"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            />
+                            <MDBInput type="text" label="Company Name" value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                            <MDBInput type="text" label="Job Title" value={jobTitle} onChange={(e) => setTitle(e.target.value)} />
+                            <MDBInput type="text" label="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                            <MDBInput type="text" label="Company Link" value={companyLink} onChange={(e) => setCompanyLink(e.target.value)} />
                         </form>
                     </MDBModalBody>
                     <MDBModalFooter className="justify-content-center">
@@ -152,14 +84,14 @@ function Dashboard(props) {
                             color="info"
                             onClick={() => {
                                 toggleModal();
-                                addEvent();
+                                addJob();
                             }}
                         >
                             Add
                         </MDBBtn>
                     </MDBModalFooter>
                 </MDBModal>
-            </React.Fragment> */}
+            </React.Fragment>
         </div>
 
 
