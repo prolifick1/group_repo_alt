fd05496242052a261f0d2ea471ba8d78c4ffe003
Luis Manzo <manzosk3@gmail.com>
2022-08-25 10:18:22 -0500

    resolving merge conflicts

--- back_end/back_app/admin.py
+++ back_end/back_app/admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-<<<<<<< HEAD
 from .models import AppUser, AppliedJobs, Interview, Posts
-=======
-from .models import AppUser, AppliedJobs, Interview, Post
->>>>>>> bd42311 (added changes)
 
 admin.site.register(AppUser, UserAdmin)
 admin.site.register(AppliedJobs)
 admin.site.register(Interview)
-<<<<<<< HEAD
 admin.site.register(Posts)
-=======
-admin.site.register(Post)
->>>>>>> bd42311 (added changes)
